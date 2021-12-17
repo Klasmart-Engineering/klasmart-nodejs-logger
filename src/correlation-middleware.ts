2213cfe28b5eae58a35d18c6f138803797346894
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-12-17 12:29:38 +0900

    feat(H5P-341): automatically add x-correlation-header when correlation-middleware initialized

--- src/correlation-middleware.ts
+++ src/correlation-middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { AsyncLocalStorage } from 'async_hooks';
+import http, { IncomingMessage } from 'http';
+import https, { RequestOptions } from 'https';
 
 export const DEFAULT_CORRELATION_HEADER = 'x-correlation-id'
 
@@ -28,3 +30,55 @@ const generateCorrelationId = () => uuidv4();
 export function withCorrelation() {
     return localStorage.getStore();
 }
+
+/**
+ * Adds hooks to http/https modules to automatically attach a correlation
+ * ID to outgoing requests
+ */
+
+function attachCorrelationIDHook(module: typeof http | typeof https) {
+    let actualCall = module.request;
+
+    function request(options: RequestOptions | string | URL, callback?: (res: http.IncomingMessage) => void): http.ClientRequest;
+    function request(url: string | URL, options: RequestOptions, callback?: (res: http.IncomingMessage) => void): http.ClientRequest;
+    function request(
+        url: string | URL | RequestOptions, 
+        options: RequestOptions | ((res: http.IncomingMessage) => void) | undefined,
+        callback?: (res: http.IncomingMessage) => void | undefined
+    ) {
+        // Logic around painful overloading
+        let actualUrl: string | URL;
+        let actualOptions: RequestOptions;
+        let actualCallback: ((res: http.IncomingMessage) => void) | undefined;;
+        
+        if (url instanceof URL || typeof url === 'string') {
+            actualUrl = url;
+            actualOptions = options as RequestOptions;
+            actualCallback = callback;
+        } else {
+            actualOptions = url;
+            actualCallback = options as (res: http.IncomingMessage) => void;
+        }
+
+        // Get correlation ID
+        const correlationId = withCorrelation();
+
+        // Case: Headers object is present but no correlationID provided
+        if (actualOptions.headers && !actualOptions.headers[DEFAULT_CORRELATION_HEADER]) {
+            actualOptions.headers[DEFAULT_CORRELATION_HEADER] = correlationId;
+        }
+
+        // Case: Headers option was not passed, add both headers option and correlationID
+        if (!actualOptions.headers) {
+            actualOptions.headers = {};
+            actualOptions.headers[DEFAULT_CORRELATION_HEADER] = correlationId;
+        }
+
+        // Delegate to wrapped request call
+        return actualCall(actualOptions, actualCallback)
+    }
+    module.request = request;
+}
+
+attachCorrelationIDHook(http);
+attachCorrelationIDHook(https);
